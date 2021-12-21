63c4d3086b011cfb13ebea43585c3e27e35ca32b
Елена <@->
2021-12-21 19:53:34 +0300

    Изменение 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Cas
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Asc();
+        }
+        static void Asc()
+        {
+            string WantPlay;
+            Console.WriteLine("You want play???");
+            WantPlay = Console.ReadLine();
+            if (WantPlay == "Yes")
+            {
+                Game();
+            }
+            else
+            {
+                Console.WriteLine("Good Bye");
+            }
+        }
+        static void Game()
+        {
+            int WinNum;
+            int UserNum;
+            Console.WriteLine("Your num is...");
+            Random rnd = new Random();
+            WinNum =rnd.Next(10);
+            UserNum = Convert.ToInt32(Console.ReadLine());
+            if (UserNum == WinNum)
+            {
+                Console.WriteLine("You Win!!!");
+                Asc();
+            }
+            else
+            {
+                Console.WriteLine("You Luse");
+                Console.WriteLine("Win Num is:" + WinNum);
+                Asc();
+            }
+
+        }
+    }
+}
